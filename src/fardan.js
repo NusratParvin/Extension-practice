826944a76a57e21e8cb8b5f410a63fd5523acc3f
NusratParvin <nusrat.official@gmail.com>
2025-05-30 18:17:04 +0400

    fardan

--- src/fardan.js
+++ src/fardan.js
@@ -6,14 +6,33 @@ puppeteer.use(stealthPlugin());
 const browser = await puppeteer.launch();
 const page = await browser.newPage();
 
-async function getFardanRate(targetCode) {
+async function getFardanRate(currencyCode) {
   await page.goto("https://alfardanexchange.com/foreign-exchange", {
     waitUntil: "networkidle2",
   });
 
-  await page.waitForSelector("#myTable tbody tr");
+  const selector = "#myTable tbody tr";
+  await page.waitForSelector(selector);
+
+  const rate = await page.evaluate((targetCode) => {
+    const rows = document.querySelectorAll("#myTable tbody tr");
+
+    for (const row of rows) {
+      const cells = row.querySelectorAll("td");
+      const currencyCodeCell = cells[0]?.textContent.trim();
+
+      if (currencyCodeCell === targetCode) {
+        const buy = cells[2].textContent.trim();
+        const sell = cells[3].textContent.trim();
+        return { buy, sell };
+      }
+    }
+    return null;
+  }, currencyCode);
+
+  return rate;
 }
 
-getFardanRate("CAD")
+getFardanRate("BDT")
   .then((rate) => console.log("rate:", rate))
   .catch(console.error);
